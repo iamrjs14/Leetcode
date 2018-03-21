2aa3a41f2561c79f61668226d759c4fc9f03c579
iamrjs14 <rushabhjshah3@gmail.com>
2018-03-20 23:23:56 -0500

    Done

---
+++ src/MaximumGuest.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MaximumGuest {
+
+    void findGuest(List<Integer[]> lst) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < lst.size(); i++) {
+            Integer[] a1 = lst.get(i);
+            map.put(i, a1[1] - a1[0]);
+        }
+        List<Integer> lst2 = map.values().stream().sorted().collect(Collectors.toList());
+        System.out.println(lst2.get(lst.size()-1));
+    }
+
+    public static void main(String[] args) {
+
+        MaximumGuest mg = new MaximumGuest();
+        List<Integer[]> lst = new ArrayList<Integer[]>();
+        Integer[] a1 = {1, 4};
+        Integer[] a2 = {2, 4};
+        lst.add(a1);
+        lst.add(a2);
+        mg.findGuest(lst);
+
+    }
+}
